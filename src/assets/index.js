0bbcae20f4bc48ac5bd60e1352a24cc5c2250c0b
ausieinsead <ausie.widawati@insead.edu>
2026-05-10 22:17:53 +0800

    add logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,30 +7,43 @@ import linkedin from "./linkedin.png";
 import gmail from "./gmail.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import logo from "./logo.svg";
+import herobg from "./herobg.svg";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import kpmg from "./kpmg.png";
+import stanford from "./stanford.png";
+import inseadLogo from "./logo-insead.jpg";
+import ausieFws from "./ausie fws.png";
+import mealnus from "./mealnus.png";
+import bitcoin from "./bitcoin.png";
+import carms from "./carms.jpg";
+
+import figmaSvg from "./Figma.svg";
+import jira from "./Jira.svg";
+import ntu from "./NTU.svg";
+import postgresql from "./PostgresSQL.svg";
+import python from "./Python.svg";
 
 import css from "./tech/css.png";
-import mysql from "./tech/mysql.svg";
-import rlanguage from "./tech/rlanguage.png";
-import tableau from "./tech/tableau.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
+import mysql from "./tech/mysql.svg";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
+import rlanguage from "./tech/rlanguage.png";
+import tableau from "./tech/tableau.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
 
 import UOB from "./company/uob.png";
 import optimum from "./company/optimum.png";
-
-import mealnus from "./mealnus.png";
-import bitcoin from "./bitcoin.png";
-import carms from "./carms.jpg";
 
 export {
   backend,
@@ -42,25 +55,38 @@ export {
   gmail,
   menu,
   close,
+  logo,
+  herobg,
+  jobit,
+  tripguide,
+  kpmg,
+  stanford,
+  inseadLogo,
+  ausieFws,
+  mealnus,
+  bitcoin,
+  carms,
+  figmaSvg,
+  jira,
+  ntu,
+  postgresql,
+  python,
   css,
-  mysql,
-  rlanguage,
-  tableau,
   docker,
   figma,
   git,
   html,
   javascript,
   mongodb,
+  mysql,
   nodejs,
   reactjs,
   redux,
+  rlanguage,
+  tableau,
   tailwind,
-  typescript,
   threejs,
-  optimum,
+  typescript,
   UOB,
-  mealnus,
-  bitcoin,
-  carms,
+  optimum,
 };
